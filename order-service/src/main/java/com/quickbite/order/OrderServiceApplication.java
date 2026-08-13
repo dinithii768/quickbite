02d67107d77abfb84e6c7dd60152722417ff6821
Sharadha Pathirana <dinithisharadha2002@gmail.com>
2026-08-13 22:12:04 +0530

    chore: setup project folder structure and pom.xml for all services

---
+++ order-service/src/main/java/com/quickbite/order/OrderServiceApplication.java
@@ -0,0 +1,20 @@
+package com.quickbite.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * QuickBite Order Service
+ * Main application entry point.
+ *
+ * Owner: Member 2
+ * Responsibility: Cart and Order Management
+ */
+@SpringBootApplication
+public class OrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+}
